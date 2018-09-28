cafd0a0f99dc36d5fddceb3955d2dd9941f1260e
marvin_nascimento_accenture <marvin.do.nascimento@CPX-8A41XA9REBU>
2018-09-28 15:12:48 -0300

    Volta a versão para o dapper

---
+++ Tests/Person.UnitTests/Infrastructure/PersonRepositoryTests.cs
@@ -0,0 +1,15 @@
+﻿using Person.Domain.Repositories;
+using Person.Infrastructure.Repositories;
+
+namespace Person.UnitTests.Infrastructure
+{
+    public class PersonRepositoryTests
+    {
+        private readonly IPersonRepository _personRepository = new PersonRepository();
+
+        public PersonRepositoryTests()
+        {
+
+        }
+    }
+}
